3ef0e80695f5edca2257e24a12e05563bb440304
wendy81 <457699029@qq.com>
2017-05-04 19:22:41 +0800

    ADD HtmlWebpackPlugin CommonsChunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+
 module.exports = {
-  entry: './index',
-  output: {
-    filename: 'browser-bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
+    context: resolve(__dirname),
+    entry: {
+      app: './index.js',
+      vendor: ['react', 'react-dom']
+    },
+    output: {
+        filename: 'browser-bundle.js',
+        path: 'docs'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+              presets: ['es2015', 'react']
+          }
         }
-      },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template: __dirname + '/index.html'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.js'
+        })
     ]
-  }
 };
