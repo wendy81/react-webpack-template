edf831a33725be280ff87227fe5e2c8e665e44ae
wendy81 <457699029@qq.com>
2017-05-04 19:30:12 +0800

    add browser-bundle.js

---
+++ docs/browser-bundle.js
@@ -0,0 +1,18 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var React = __webpack_require__(1);
+	var ReactDOM = __webpack_require__(158);
+	
+	ReactDOM.render(React.createElement(
+	  'h1',
+	  null,
+	  'Hello React!'
+	), document.getElementById('root'));
+
+/***/ })
+]);
+//# sourceMappingURL=browser-bundle.js.map
